7c58331d2e79b8ef6f0a9ff3524401359db29515
Soma Lucz <soma.lucz@diplomatiq.org>
2020-04-07 22:35:05 +0200

    chore: Config application, set up CI

---
+++ src/main/java/org/diplomatiq/diplomatiqbackend/DiplomatiqBackendApplication.java
@@ -0,0 +1,13 @@
+package org.diplomatiq.diplomatiqbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomatiqBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomatiqBackendApplication.class, args);
+	}
+
+}
