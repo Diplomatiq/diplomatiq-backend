800a64daea429ed1af2ea4a4319a0361deaf6531
Soma Lucz <soma.lucz@diplomatiq.org>
2020-04-11 11:16:53 +0200

    feat: Redirect to website on visiting the root

--- src/main/java/org/diplomatiq/diplomatiqbackend/methods/controllers/UnauthenticatedMethods.java
+++ src/main/java/org/diplomatiq/diplomatiqbackend/methods/controllers/UnauthenticatedMethods.java
@@ -1,12 +1,26 @@
 package org.diplomatiq.diplomatiqbackend.methods.controllers;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UnauthenticatedMethods {
+
+    @RequestMapping(
+        name = "rootRedirect",
+        path = "",
+        method = RequestMethod.GET
+    )
+    public ResponseEntity rootRedirect() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "https://www.diplomatiq.org");
+        return new ResponseEntity<>(headers, HttpStatus.TEMPORARY_REDIRECT);
+    }
 
     @RequestMapping(
         name = "challengeV1",
