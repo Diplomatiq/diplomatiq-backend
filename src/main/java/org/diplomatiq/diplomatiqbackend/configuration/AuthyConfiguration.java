7dde8da403e8b7ca1f6ddc26b9855503ebb2556a
Soma Lucz <soma.lucz@diplomatiq.org>
2020-04-21 22:49:48 +0200

    feat: Add initial support for 2FA via Authy

---
+++ src/main/java/org/diplomatiq/diplomatiqbackend/configuration/AuthyConfiguration.java
@@ -0,0 +1,17 @@
+package org.diplomatiq.diplomatiqbackend.configuration;
+
+import com.authy.AuthyApiClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AuthyConfiguration {
+    @Value("${AUTHY_API_KEY}")
+    private String AUTHY_API_KEY;
+
+    @Bean
+    public AuthyApiClient authyApiClient() {
+        return new AuthyApiClient(AUTHY_API_KEY);
+    }
+}
