40b912864d7968db8fd30487c36534497df3e58d
Soma Lucz <soma.lucz@diplomatiq.org>
2020-05-07 18:22:47 +0200

    refactor: Remove unnecessary SRP parameter getter

--- src/main/java/org/diplomatiq/diplomatiqbackend/utils/crypto/srp/RequestBoundaryCrossingSRP6Server.java
+++ src/main/java/org/diplomatiq/diplomatiqbackend/utils/crypto/srp/RequestBoundaryCrossingSRP6Server.java
@@ -1,7 +1,6 @@
 package org.diplomatiq.diplomatiqbackend.utils.crypto.srp;
 
 import org.bouncycastle.crypto.agreement.srp.SRP6Server;
-import org.bouncycastle.crypto.agreement.srp.SRP6Util;
 
 import java.math.BigInteger;
 
@@ -16,9 +15,5 @@ public class RequestBoundaryCrossingSRP6Server extends SRP6Server {
 
     public void setB(BigInteger B) {
         this.B = B;
-    }
-
-    public BigInteger getM1() {
-        return SRP6Util.calculateM1(this.digest, this.N, this.A, this.B, this.S);
     }
 }
